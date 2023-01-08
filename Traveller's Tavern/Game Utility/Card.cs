097d0ff1e69cfd935a8d3e9799a9ce677bd7fdb9
Zander <u1373936@umail.utah.edu>
2023-01-07 23:37:58 -0700

    Changed different types of cards to be their own classes extending the Card class

--- Traveller's Tavern/Game Utility/Card.cs
+++ Traveller's Tavern/Game Utility/Card.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static gameutil.SometimesCard;
 
 namespace gameutil
 {
@@ -13,8 +14,6 @@ namespace gameutil
         public string Title { get; private set; }
 
         public string Description { get; private set; }
-
-        public CardType Type { get; private set; }
 
         public CardEffect Effect { get; private set; }
 
@@ -28,14 +27,8 @@ namespace gameutil
 
         public int GoldEffect { get; private set; }
 
-        // GAMBLING CARD INFO =======================================
-        // Information for the case in which this is a gambling card
-
-        public GamblingType GamblingType { get; private set; }
-
         public Card()
         {
-            Type = CardType.Anytime;
             Title = "Default Card";
             Description = "Default Description";
 
@@ -44,9 +37,8 @@ namespace gameutil
             GoldEffect = 0;
         }
 
-        public Card(CardType type, string title, string description, int vitalEffect, int intoxEffect, int goldEffect)
+        public Card(string title, string description, int vitalEffect, int intoxEffect, int goldEffect)
         {
-            Type = type;
             Title = title;
             Description = description;
 
@@ -56,8 +48,75 @@ namespace gameutil
         }
     }
 
+    public class SometimesCard : Card
+    {
+        public delegate bool CardConditionChecker(Card card);
+
+        public CardConditionChecker CheckCardConditions { get; private set; }
+
+        /// <summary>
+        /// Default constructor for a new Sometimes Card. Used only for serializing a SometimesCard from data.
+        /// Sets the CheckCardConditions to throw an exception, as that member should be overrided during
+        /// serialiazation - if it's not, some error has occurred.
+        /// </summary>
+        /// <exception cref="SometimesCardLoadingException"></exception>
+        public SometimesCard() : base()
+        {
+            CheckCardConditions = (Card card) => throw new SometimesCardLoadingException();
+        }
+
+        public SometimesCard(string title, string description, int vitalEffect,
+            int intoxEffect, int goldEffect, CardConditionChecker checkCardConditions) :
+            base(title, description, vitalEffect, intoxEffect, goldEffect)
+        {
+            CheckCardConditions = checkCardConditions;
+        }
+    }
+
+    public class GamblingCard : Card
+    {
+        public GamblingType GamblingType { get; private set; }
+
+        // TODO: Gonna need a default constructor here chief
+
+        public GamblingCard(string title, string description, int vitalEffect,
+            int intoxEffect, int goldEffect, GamblingType gamblingType) :
+            base(title, description, vitalEffect, intoxEffect, goldEffect)
+        {
+            GamblingType = gamblingType;
+        }
+
+        public bool CheckForControl(GamblingCard currentCard)
+        {
+            // If this card is an In card, it takes control if the most
+            // recently played gambling card is In or Cheating
+            if (GamblingType == GamblingType.In
+                && (currentCard.GamblingType == GamblingType.In
+                || currentCard.GamblingType == GamblingType.Cheating))
+                return true;
+
+            // If this card is a Cheating card, it takes control if the most
+            // recently played gambling card is In, Winning Hand, or Cheating
+            if (GamblingType == GamblingType.Cheating
+                && (currentCard.GamblingType == GamblingType.In
+                || currentCard.GamblingType == GamblingType.Cheating
+                || currentCard.GamblingType == GamblingType.WinningHand))
+                return true;
+
+            // If this card is a Cheating card, it takes control if the most
+            // recently played gambling card is In or Cheating
+            if (GamblingType == GamblingType.WinningHand
+                && (currentCard.GamblingType == GamblingType.In
+                || currentCard.GamblingType == GamblingType.Cheating))
+                return true;
+
+            return false;
+        }
+    }
+
     public enum CardType
     {
+        Action,
         Sometimes,
         Anytime,
         Gambling
@@ -81,5 +140,24 @@ namespace gameutil
         In,
         Cheating,
         WinningHand
+    }
+
+    public class SometimesCardLoadingException : Exception
+    {
+        public SometimesCardLoadingException() 
+            : base("Data for a sometimes card was not loaded correctly. " +
+                  "Possible corrupted data or error in a library.")
+        {
+        }
+
+        public SometimesCardLoadingException(string message)
+            : base(message)
+        {
+        }
+
+        public SometimesCardLoadingException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
     }
 }
