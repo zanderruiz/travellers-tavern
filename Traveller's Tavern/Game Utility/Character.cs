bd817977df909c83ce8b37f478f8d54cc058b6c2
Zander <u1373936@umail.utah.edu>
2023-01-09 18:13:58 -0700

    Constructors added

--- Traveller's Tavern/Game Utility/Character.cs
+++ Traveller's Tavern/Game Utility/Character.cs
@@ -9,8 +9,34 @@ namespace gameutil
 {
     public class Character
     {
-        public List<Card> CardDeck;
+        public string Name { get; private set; }
 
-        public string Description;
+        public string Description { get; private set; }
+
+        public List<Card> CardDeck { get; private set; }
+
+        /// <summary>
+        /// Constructs a default Character, used for deserialization.
+        /// </summary>
+        public Character()
+        {
+            Name = "Default Name";
+            Description = "Default Description";
+            CardDeck= new List<Card>();
+        }
+
+        /// <summary>
+        /// Constructs a new Character with the given name, description, and
+        /// card deck.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="description"></param>
+        /// <param name="cardDeck"></param>
+        public Character(string name, string description, List<Card> cardDeck)
+        {
+            Name = name;
+            Description = description;
+            CardDeck = cardDeck;
+        }
     }
 }
