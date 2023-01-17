21aa757d521930a2dd51a1c2e7c5f5fd9578c5b0
Zander <u1373936@umail.utah.edu>
2023-01-16 19:14:01 -0700

    ActionQueue class created

---
+++ Traveller's Tavern/Main Game Controller/ActionQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maingamecontroller
+{
+    public class ActionQueue
+    {
+        public delegate void Action();
+
+        private LinkedList<Action> Queue = new LinkedList<Action>();
+
+        private Stack<Action> PastActions = new Stack<Action>();
+
+        public void TakeNextAction()
+        {
+            Queue.First()();
+
+            PastActions.Push(Queue.First());
+
+            Queue.RemoveFirst();
+        }
+
+        public void QueueAction(Action action)
+        {
+            Queue.AddLast(action);
+        }
+
+        public void QueueActionNext(Action action)
+        {
+            if (Queue.First != null) Queue.AddAfter(Queue.First, action);
+
+            else Queue.AddFirst(action);
+        }
+
+        public void UndoLastAction(Action action)
+        {
+
+        }
+    }
+}
