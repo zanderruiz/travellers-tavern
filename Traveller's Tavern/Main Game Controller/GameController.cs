35266dd141a74b876f8e8edca81e31e84eccf78a
Zander <u1373936@umail.utah.edu>
2023-01-19 23:28:50 -0700

    Updated with very intiial code.

--- Traveller's Tavern/Main Game Controller/GameController.cs
+++ Traveller's Tavern/Main Game Controller/GameController.cs
@@ -4,6 +4,12 @@ namespace maingamecontroller
 {
     public class GameController
     {
+        private Tavern tavern = new Tavern();  
+        private Stack<Tavern> saveStates = new Stack<Tavern>();
 
+        public AddPlayer(Player player)
+        {
+
+        }
     }
 }
